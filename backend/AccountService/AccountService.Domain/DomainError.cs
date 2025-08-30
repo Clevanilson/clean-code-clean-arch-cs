6ae8a5145db1dae5a8c4a882ca0c3fbf056fe74a
Clevanilson <clevanilson.contato@gmail.com>
2025-08-30 12:14:48 -0300

    feat(account): implements `IDomainError` to `DomainError`

--- backend/AccountService/AccountService.Domain/DomainError.cs
+++ backend/AccountService/AccountService.Domain/DomainError.cs
@@ -1,6 +1,10 @@
+using AccountSerivice.Domain.Interfaces;
+
 namespace AccountService.Domain;
 
-public class DomainError : Exception
+public class DomainError : Exception, IDomainError
 {
+    public int Code => 400;
+    public string Action => "Please check the request data and try again.";
     public DomainError(string message) : base(message) { }
 }
